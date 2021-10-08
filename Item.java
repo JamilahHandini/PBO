414f0bfb4fa9049ee7d7dfc354bf6abf629a3808
JamilahHandini <jamilahhandini8@gmail.com>
2021-10-08 09:15:01 +0700

    Tugas 4

---
+++ Item.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PBO;
+
+/**
+ *
+ * @author ASUS PRO
+ */
+public class Item {
+private String name;
+
+public Item() {
+name = "Ipin";
+}
+
+public Item(String name) {
+
+System.out.println(this.name= "Ipin");
+}
+}

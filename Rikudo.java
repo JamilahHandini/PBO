e39ee4972942ec6d0dd1626dffc6015c1887a6d0
JamilahHandini <jamilahhandini8@gmail.com>
2021-10-23 23:45:18 +0700

    Week 5

---
+++ Rikudo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PBO;
+
+/**
+ *
+ * @author ASUS PRO
+ */
+public class Rikudo {
+    protected String Dojutsu = "Rinnegan";
+    protected String Senjutsu = "Sage Mode";
+
+}

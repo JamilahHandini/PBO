1639ddb95db5636650d1421f7d72dc1eaa4618d1
JamilahHandini <80727419+JamilahHandini@users.noreply.github.com>
2021-11-03 21:30:52 +0700

    Week 7

--- Circle.java
+++ Circle.java
@@ -1,3 +1,68 @@
+<<<<<<< HEAD
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PBO;
+
+/**
+* The Circle class models a circle with a radius and color.
+*/
+public class Circle { // Save as "Circle.java"
+// private instance variable, not accessible from outside this class
+private double radius;
+private String color; // ADDED: variable String color is already here
+
+
+// Constructors (overloaded)
+/** Constructs a Circle instance with default value for radius and color */
+public Circle() { // 1st (default) constructor
+radius = 1.0;
+color = "red";
+}
+/** Constructs a Circle instance with the given radius and default color */
+public Circle(double r) { // 2nd constructor
+radius = r;
+color = "red";
+}
+
+//ADDED
+/** Constructs a Circle instance with the given radius and given color */
+public Circle(double r,String c) { // 2nd constructor
+radius = r;
+color = c;
+}
+
+/** Returns the radius */
+public double getRadius() {
+return radius;
+}
+
+//ADED setter for Color
+public void setColor(String color){
+    this.color=color;
+}
+
+//ADED getter for Color
+public String getColor(){
+    return color;
+}
+
+/** Returns the area of this Circle instance */
+public double getArea() {
+return radius*radius*Math.PI;
+}
+/** Return a self-descriptive string of this instance in the form of
+Circle[radius=?,color=?] */
+public String toString() {
+return "Circle[radius=" + radius + " color=" + color + "]";
+}
+
+
+}
+=======
 public class Circle extends Shape {
     
         private double radius;
@@ -34,3 +99,4 @@ return "A Circle with radius= " +radius+ " which is a subclass of " + super.toSt
 }
 
 }
+>>>>>>> 60aa0edac4d21191e8eeced8e6fbdcaa70efccc1

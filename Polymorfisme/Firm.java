a9dc73e57226f118e697f3ec7345376cd4c7ce7c
JamilahHandini <80727419+JamilahHandini@users.noreply.github.com>
2021-11-12 09:12:49 +0700

    Week 8

---
+++ Polymorfisme/Firm.java
@@ -0,0 +1,14 @@
+
+
+package Polymorfisme;
+
+/**
+ *
+ * @author ASUS PRO
+ */
+public class Firm {
+    public static void main(String[] args){
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+}
